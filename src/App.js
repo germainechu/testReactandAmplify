bc16fc175d48460d4c54f947ca92b75acc3dfe24
germaine.chu <g0xchu@gmail.com>
2024-03-28 16:02:15 -0700

    testing routes on the / endpoint

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <Router> {/* Wrap your Routes in a Router component */}
         <Routes> 
+        <Route path="/" element={<img src="https://openseauserdata.com/files/7d5d665f92c09858439458678bc879a4.gif"></img>}></Route>
           <Route path="/skill/:id" element={<Skill/>}></Route>
         </Routes>
       </Router>
